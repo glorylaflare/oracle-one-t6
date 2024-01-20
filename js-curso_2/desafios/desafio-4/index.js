1c189c2876e373159c96fde3275124094cd4b84c
Marcelo Gonçalves <juninho_toh@hotmail.com>
2024-01-20 20:00:02 -0300

    feat: adicionando desafio 4

---
+++ js-curso_2/desafios/desafio-4/index.js
@@ -0,0 +1,18 @@
+//1
+let listaGenerica = [];
+
+//2
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+//3 
+linguagensDeProgramacao.push('Java');
+linguagensDeProgramacao.push('Ruby');
+linguagensDeProgramacao.push('GoLang');
+
+//4
+let listaNomes = ['Marcelo', 'Rebeca', 'Renan'];
+console.log(listaNomes[0])
+//5
+console.log(listaNomes[1])
+//6
+console.log(listaNomes[2])
